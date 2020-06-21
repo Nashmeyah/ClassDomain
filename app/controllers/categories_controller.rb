3dec0dfddffaa3ef5a63013e544654d146114af7
Nashmeyah <nashalrekabi@gmail.com>
2020-06-20 20:50:01 -0600

    Created Admin functions for category, now working on adding courses to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,38 +3,37 @@ before_action :authenticate_user!
 before_action :set_category, only: [ :show, :edit, :update, :destroy]
 
     def index
-        if user_signed_in?
-            @category = Category.all
-        end
+        @category = Category.all
     end
 
     def new
-        if user_signed_in?
-            @category = Category.new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(name: params[:category][:name])
+        if @category.valid?
+            @category.save
+            redirect_to @category
+        else 
+            render :new
         end
     end
 
     def show
-        if user_signed_in?
-             render :show
-        end
-        # flash[:error]"You must sign in or create an account to view this page"
-    end
-
-    def create
-        @category = Category.new(name: params[:name])
     end
 
     def edit
     end
 
     def update
-        @category.update(name: params[:name])
+        @category.update(name: params[:category][:name])
         redirect_to category_path(@category)
     end
 
-    def delete
+    def destroy
         @category.destroy
+        redirect_to categories_path
     end
 
     private
